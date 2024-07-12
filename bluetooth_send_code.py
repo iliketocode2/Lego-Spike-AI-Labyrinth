d3037f6254cd1b94ea65fc1fbbd780ca6ed1650b
William Goldman <60369048+iliketocode2@users.noreply.github.com>
2024-07-12 11:27:27 -0400

    semi working bluetooth

---
+++ bluetooth_send_code.py
@@ -0,0 +1,46 @@
+import sys
+print(sys.path)
+from BLE_CEEO import Yell, Listen
+
+def peripheral(name): 
+    try:
+        p = Yell(name, verbose = True)
+        if p.connect_up():
+            print('P connected')
+            time.sleep(2)
+            payload = ''  
+            for i in range(100):
+                payload += str(i)
+                p.send(payload)
+                if p.is_any:
+                    print(p.read())
+                if not p.is_connected:
+                    print('lost connection')
+                    break
+                time.sleep(1)
+    except Exception as e:
+        print(e)
+    finally:
+        p.disconnect()
+        print('closing up')
+        
+                
+def central(name):   
+    try:   
+        L = Listen(name, verbose = True)
+        if L.connect_up():
+            print('L connected')
+            while L.is_connected:
+                time.sleep(4)
+                if L.is_any:
+                    reply = L.read()
+                    print(reply) #seems to stop at 80 characteres
+                    L.send(reply[:20])  #seems to stop around 20 characters
+    except Exception as e:
+        print(e)
+    finally:
+        L.disconnect()
+        print('closing up')
+
+peripheral('Maria')
+#central('Maria')
--- main.py
+++ main.py
@@ -1,38 +1,132 @@
-from pyscript import window, document, display
-from webBLE import BLE
-import asyncio, struct
+from pyscript import document, window, when
+import andrea_terminal, restapi, file_transfer, file_os
+from ble_test import code as test_code
+import json
 
-fred = BLE('Maria')
-s = document.getElementById('text')
-s.innerText = 'connecting up'
+ARDUINO_NANO = 128
+SPIKE = 256
+path = "https://raw.githubusercontent.com/chrisbuerginrogers/SPIKE_Prime/main/BLE/BLE_CEEO.py"
 
-async def connectIt(event):
-    success = await fred.ask(None)
-            
-async def disconnectIt(event):
-    print('disconnecting')
-    success = await fred.disconnect()
+def on_connect(event):
+    if terminal.connected:
+        connect.innerText = 'connect up'
+        await terminal.board.disconnect()
+    else:
+        await terminal.board.connect('repl')
+        if terminal.connected:
+            connect.innerText = 'disconnect'
+            await file_os.getList(terminal, list)
+            await terminal.eval('\x03', True)
+    for b in btns:
+        b.disabled = not terminal.connected
 
-async def readIt(event):
-    payload = await fred.read()
-    s.innerText = ' up'
-    if payload:
-        s.innerText = ' up ' + str(len(payload))
-        print(payload)
-        acc = []
-        t = []
-        for i in range(0, len(payload), 4):
-            print(i)
-            #t.append(payload[i]*255+payload[i+1])
-            #acc.append(payload[i+2]*255+payload[i+3])
-        #display(t)
-            
-        '''array=[int(c) for c in payload]
-        print(array)    
+def on_disconnect():
+    connect.innerText = 'connect up'
+    python.code = ''
+    list.options.length = 0
+    terminal.update(0)
 
-        time, acc = struct.unpack('>hh',payload)
-        print(time, acceleration)
-        #s.innerText = numbers'''
+async def on_load(event):
+    if terminal.connected:
+        name = path.split('/')[-1]
+        print('path, name: ',path,name)
+        reply = await restapi.get(path)
+        status = await terminal.download(name,reply)
+        if not status: 
+            window.alert(f"Failed to load {name}")  
+    else:
+        window.alert('connect to a processor first')
+
+def handle_board(event):
+    code = event.code
+    if terminal.connected:
+        await terminal.paste(code)
+        terminal.focus()
+        return False  # return False to avoid executing on browser
+    else:
+        return True
+
+def on_select(event):
+    python.code = await file_os.read_code(terminal, list)
+
+def on_clear(event):
+    terminal.board.terminal.clear()
+
+@when("click", "#test")
+def on_test(event):
+    python.code = test_code
+
+@when("click", "#CtrlC")
+def send_CtrlC(event):
+    await terminal.board.write('\x03')
+
+path = "https://raw.githubusercontent.com/micropython/micropython-lib/master/micropython/umqtt.simple/umqtt/simple.py"
+
+@when("click","#library2")
+async def load_umqtt(event):
+    if terminal.connected:
+        data = await restapi.get(path)
+        status = await terminal.download('mqtt.py',data)
+        if not status: 
+            window.alert(f"Failed to load {name}")  
+    else:
+        window.alert('connect to a processor first')
 
 
+connect      = document.getElementById('connect')
+library      = document.getElementById('library')
+library2     = document.getElementById('library2')
+progress_bar = document.getElementById('progress')
+list         = document.getElementById('files')
+python       = document.getElementById('mpCode')
+remote       = document.getElementById('upload')
+clear        = document.getElementById('clear')
+ctrlC        = document.getElementById('CtrlC')
+
+connect.onclick = on_connect
+library.onclick = on_load
+#list.onchange = on_select
+remote.onclick = on_select
+clear.onclick = on_clear
+python.handleEvent = handle_board
+
+#terminal = andrea_terminal.Terminal()
+terminal = file_transfer.Ampy(ARDUINO_NANO, progress_bar)
+terminal.disconnect_callback = on_disconnect
+
+btns = [library, library2, remote, clear, ctrlC]
+for b in btns:
+    b.disabled = not terminal.connected
+
+#--------------------------- all BLE control --------------------------------
+from pyscript.js_modules import ble_library
+
+ble_info = document.getElementById("ble_info")
+ble_connected = document.getElementById("ble_connected")
+
+def received_ble(data):
+    document.getElementById("ble_answer").innerHTML = 'received: '+data
+    if myClient.connected and ble.connected and push.checked:
+        myClient.publish(pub_topic.value, data)
         
+ble = ble_library.newBLE()
+ble.callback = received_ble
+
+@when("click", "#ble_connect")
+async def ask(event):
+    name = document.getElementById("ble_name").value
+    if await ble.ask(name):
+        print('name ',name)
+        await ble.connect() 
+        print('connected!')
+        ble_connected.innerHTML = 'connected'
+        
+@when("click", "#ble_disconnect")
+async def on_disconnect(event):
+    await ble.disconnect()
+    print('disconnected')
+
+@when("click", "#send_ble")
+def on_send_ble(event):
+    print(ble_info.value)
+    ble.write(ble_info.value)
